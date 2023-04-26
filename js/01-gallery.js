14c231ae8199e8633258153ab1d7c9c0686ad110
Vikki2Rikki <119513480+Vikki2Rikki@users.noreply.github.com>
2023-04-26 22:35:19 +0300

    add markup task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryItemsMarcup = createGalleryItems(galleryItems);
+
+function createGalleryItems(galleryItems){
+   return galleryItems.map(({ preview, original, description }) => {
+      return  `
+      <li class="gallery__item">
+   <a class="gallery__link"${ original }">
+   <img
+      class="gallery__image"
+      src="${ preview }"
+       data-source="${ preview }"
+      alt="${ description }"
+     />
+   </a>
+ </li> 
+      `;
+   })
+   .join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarcup);
+
