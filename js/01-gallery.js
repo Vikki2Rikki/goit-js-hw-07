60cb6e8e28b39d772efb0686f2cf48ac710f76a6
Vikki2Rikki <119513480+Vikki2Rikki@users.noreply.github.com>
2023-04-28 01:31:36 +0300

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const galleryContainer = document.querySelector('.gallery');
-
 const galleryItemsMarcup = createGalleryItems(galleryItems);
+
+galleryContainer.addEventListener('click', onOpenOriginalImg);
+
 
 function createGalleryItems(galleryItems){
    return galleryItems.map(({ preview, original, description }) => {
@@ -15,7 +17,7 @@ function createGalleryItems(galleryItems){
    <img
       class="gallery__image"
       src="${ preview }"
-       data-source="${ preview }"
+       data-source="${ original }"
       alt="${ description }"
      />
    </a>
@@ -24,6 +26,30 @@ function createGalleryItems(galleryItems){
    })
    .join('');
 }
-
 galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarcup);
+
+function onOpenOriginalImg(e){
+   // preventDefault(e);
+   blockStandartAction(e);
+
+   if(e.target.nodeName !== "IMG"){
+      return;
+   } else{
+      const instance = basicLightbox.create(`
+      <img src="${e.target.dataset.source}"  width="1280" height="853">
+   `);
+   instance.show();
+
+galleryContainer.addEventListener('keydown', onCloseOriginalImg);
+function onCloseOriginalImg(e){
+   if(e.code === "Escape"){
+      instance.close();
+   }};
+   }
+}
+
+function blockStandartAction(e){
+   e.preventDefault()
+}
+
 
